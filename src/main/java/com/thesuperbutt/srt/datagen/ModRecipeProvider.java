ad2f318220f6583936a70316dfab17f389b550e0
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2024-01-21 00:27:12 -0600

    Downgrading to 1.20.1 to keep inline with tutorial

--- src/main/java/com/thesuperbutt/srt/datagen/ModRecipeProvider.java
+++ src/main/java/com/thesuperbutt/srt/datagen/ModRecipeProvider.java
@@ -13,6 +13,7 @@ import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
     private static final List<ItemLike> SAPPHIRE_SMELTABLES = List.of(
@@ -28,7 +29,7 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     }
 
     @Override
-    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
+    protected void buildRecipes(@NotNull Consumer<FinishedRecipe> recipeOutput) {
         oreSmelting(recipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f, 200, "sapphire");
         oreBlasting(recipeOutput, SAPPHIRE_SMELTABLES, RecipeCategory.MISC, ModItems.SAPPHIRE.get(), 0.25f, 100, "sapphire");
 
@@ -54,21 +55,21 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
             .save(recipeOutput);
     }
 
-    protected static void oreSmelting(@NotNull RecipeOutput pRecipeOutput, List<ItemLike> pIngredients,
+    protected static void oreSmelting(@NotNull Consumer<FinishedRecipe> pRecipeOutput, List<ItemLike> pIngredients,
                                       @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience,
                                       int pCookingTime, @NotNull String pGroup) {
         oreCooking(pRecipeOutput, RecipeSerializer.SMELTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_smelting");
     }
 
-    protected static void oreBlasting(@NotNull RecipeOutput pRecipeOutput, List<ItemLike> pIngredients,
+    protected static void oreBlasting(@NotNull Consumer<FinishedRecipe> pRecipeOutput, List<ItemLike> pIngredients,
                                       @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience,
                                       int pCookingTime, @NotNull String pGroup) {
         oreCooking(pRecipeOutput, RecipeSerializer.BLASTING_RECIPE, pIngredients, pCategory, pResult, pExperience, pCookingTime, pGroup, "_from_blasting");
     }
 
-    private static void oreCooking(RecipeOutput pRecipeOutput, RecipeSerializer<? extends AbstractCookingRecipe> pSerializer,
-                                   @NotNull List<ItemLike> pIngredients, RecipeCategory pCategory, ItemLike pResult, float pExperience,
-                                   int pCookingTime, String pGroup, String pSuffix) {
+    protected static void oreCooking(@NotNull Consumer<FinishedRecipe> pRecipeOutput, @NotNull RecipeSerializer<? extends AbstractCookingRecipe> pSerializer,
+                                     @NotNull List<ItemLike> pIngredients, @NotNull RecipeCategory pCategory, @NotNull ItemLike pResult, float pExperience,
+                                     int pCookingTime, @NotNull String pGroup, String pSuffix) {
         for (ItemLike itemlike : pIngredients) {
             SimpleCookingRecipeBuilder.generic(Ingredient.of(itemlike), pCategory, pResult, pExperience, pCookingTime, pSerializer)
                 .group(pGroup).unlockedBy(getHasName(itemlike), has(itemlike))
