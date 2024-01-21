3d8d5e3082bb0d79d4e999407e966dec0de7fa23
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2024-01-20 18:49:37 -0600

    EP# 14

--- src/main/java/com/thesuperbutt/srt/item/ModItems.java
+++ src/main/java/com/thesuperbutt/srt/item/ModItems.java
@@ -16,6 +16,9 @@ public class ModItems {
 
     public static final RegistryObject<Item> SAPPHIRE = ITEMS.register("sapphire",
         () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> SAPPHIRE_STAFF = ITEMS.register("sapphire_staff",
+        () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> RAW_SAPPHIRE = ITEMS.register("raw_sapphire",
         () -> new Item(new Item.Properties()));
 
