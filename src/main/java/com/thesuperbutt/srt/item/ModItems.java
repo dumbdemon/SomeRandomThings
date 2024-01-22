b55490bb432a8d0e2388ebfe34b50e38a0a13fdd
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2024-01-21 22:53:43 -0600

    Ep 26

--- src/main/java/com/thesuperbutt/srt/item/ModItems.java
+++ src/main/java/com/thesuperbutt/srt/item/ModItems.java
@@ -5,6 +5,7 @@ import com.thesuperbutt.srt.block.ModBlocks;
 import com.thesuperbutt.srt.item.custom.FuelItem;
 import com.thesuperbutt.srt.item.custom.MetalDetectorItem;
 import com.thesuperbutt.srt.item.custom.ModArmorItem;
+import com.thesuperbutt.srt.sound.ModSounds;
 import net.minecraft.world.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -61,8 +62,18 @@ public class ModItems {
     public static final RegistryObject<Item> CORN = ITEMS.register("corn",
         () -> new Item(new Item.Properties()));
 
+    public static final RegistryObject<Item> BAR_BRAWL_MUSIC_DISC = ITEMS.register("bar_brawl_music_disc",
+        () -> new RecordItem(6, ModSounds.BAR_BRAWL, new Item.Properties().stacksTo(1), 2440));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+    }
+
+    private static int getTickLength(int minutes, float secs) {
+        return Math.round(((minutes * 60) + secs) * 20);
+    }
+
+    private static int getTickLength(float secs) {
+        return Math.round(secs * 20);
     }
 }
